2bd7fa79a6edca70db2e940c8d3d042fb1a08ce2
Kevin Marin <kevinmarinsaavedra@gmail.com>
2023-11-14 14:40:30 +0100

    Testing in AppCoordinator

--- Ceiba-Software-Challenge-Tests/Coordinator/AppCoordinatorTests.swift
+++ Ceiba-Software-Challenge-Tests/Coordinator/AppCoordinatorTests.swift
@@ -8,3 +8,32 @@
 import XCTest
 @testable import Ceiba_Software_Challenge
 
+class AppCoordinatorTests: XCTestCase {
+    var sut: AppCoordinator!
+    
+    override func setUpWithError() throws {
+        sut = AppCoordinator()
+        _ = sut.start()
+    }
+    
+    override func tearDownWithError() throws {
+        sut = nil
+    }
+    
+    func test_navigation() {
+        //Arrange
+        let user: User = User()
+        let router: Route = .userDetails(user: user)
+        
+        //Act
+        sut.navigate(router)
+        
+        //Assert
+        guard let viewController = sut.navController?.topViewController as? UserListViewController else {
+            XCTFail("It's should be of type UserListViewController")
+            return
+        }
+        
+        XCTAssertNotNil(viewController, "It's should not be viewController")
+    }
+}
